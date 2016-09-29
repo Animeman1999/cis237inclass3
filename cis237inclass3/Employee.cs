9aa00822318f5ad23efb028204579d13038d6ab6
Jeffrey Martin <animeman1999@google.com>
2016-09-29 17:10:12 -0400

    Adds basic Inheritance Basic

---
+++ cis237inclass3/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass3
+{
+    class Employee
+    {
+        //Variables
+        protected string _firstname;
+        protected string _lastname;
+        protected DateTime _startDate;
+
+        //Properties
+        public string FirstName
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+            set { _startDate = value; }
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            return _firstname + " " + _lastname;
+        }
+        
+        public DayOfWeek GetStartDay()
+        {
+            return _startDate.DayOfWeek;
+        }
+
+        //Constructors
+
+        public Employee(string FirstName, string LastName, DateTime StartDate)
+        {
+            this._firstname = FirstName;
+            this._lastname = LastName;
+            this._startDate = StartDate;
+        }
+
+        public Employee() { }
+
+    }
+}
---
+++ cis237inclass3/SalaryEmployee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass3
+{
+    class SalaryEmployee : Employee
+    {
+        //Variables
+        private decimal _weeklySalary;
+
+        //Properties
+        public decimal WeeklySalary
+        {
+            get { return _weeklySalary; }
+            set { _weeklySalary = WeeklySalary; }
+        }
+        //Methods
+        public String NameAndSalary()
+        {
+            return base.ToString() + " " + _weeklySalary.ToString("C");
+        }
+
+        //Constructor
+        public SalaryEmployee(string FirstName, string LastName, DateTime StartDate, decimal WeeklySalary) 
+            //This next part calls the parent classes constructor so that we don't hae to repeat the work
+            // that the parent constructor is doing-----Note the line below is a contiuation of the line above----
+            : base(FirstName, LastName, StartDate)
+        {
+            _weeklySalary = WeeklySalary;
+        }
+
+        public SalaryEmployee() : base() { }  // make abase constructor that is a copy of the base constructor
+    }
+}
